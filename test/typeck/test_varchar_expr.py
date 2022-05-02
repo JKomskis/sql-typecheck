39bfd4af61e90e1a436520bca38df6b3d857e784
cbossut3 <cbossut21@gatech.edu>
2022-05-01 21:03:48 -0400

    oops forgot the tests

---
+++ test/typeck/test_varchar_expr.py
@@ -0,0 +1,101 @@
+import unittest
+
+from src.parsing.expr import (BinaryOp, ExprBinaryOp, ExprColumn, ExprConcat,
+                              ExprIntLiteral, ExprSubstr, ExprVarcharLiteral,
+                              expr)
+from src.types.types import BaseType, Schema, TypeMismatchError, Expression
+from src.types.symbol_table import SymbolTable
+
+
+class TestLiteral(unittest.TestCase):
+    def test_ints(self):
+        self.assertEqual(ExprVarcharLiteral("test"
+        ).type_check(SymbolTable()), Expression(Schema({}), BaseType.VARCHAR))
+        self.assertEqual(ExprVarcharLiteral("1234"
+        ).type_check(SymbolTable()), Expression(Schema({}), BaseType.VARCHAR))
+
+
+
+class TestConcat(unittest.TestCase):
+    def test_concat_literal(self):
+        self.assertEqual(ExprConcat(ExprVarcharLiteral("test"), ExprVarcharLiteral("test2")
+        ).type_check(SymbolTable()),
+        Expression(Schema({}), BaseType.VARCHAR))
+
+        self.assertEqual(ExprConcat(ExprVarcharLiteral("test"), ExprVarcharLiteral("test2")
+        ).type_check(SymbolTable()),
+        Expression(Schema({}), BaseType.VARCHAR))
+
+    def test_concat_col_literal(self):
+        st = SymbolTable({
+            "a": Schema({"col": BaseType.VARCHAR}),
+        })
+        self.assertEqual(ExprConcat(ExprVarcharLiteral("test"), ExprColumn(("a", "col"))
+        ).type_check(st),
+        Expression(Schema({"a.col": BaseType.VARCHAR}), BaseType.VARCHAR))
+
+    def test_concat_cols(self):
+        st = SymbolTable({
+            "a": Schema({"col": BaseType.VARCHAR}),
+            "b": Schema({"col": BaseType.VARCHAR}),
+        })
+        self.assertEqual(ExprConcat(ExprColumn(("b", "col")), ExprColumn(("a", "col"))
+        ).type_check(st),
+        Expression(Schema({"b.col": BaseType.VARCHAR, "a.col": BaseType.VARCHAR}), BaseType.VARCHAR))
+        # todo/issue: the schema ordering changes & matters depending on the ordering of concat
+
+    def test_concat_nested(self):
+        st = SymbolTable({
+            "a": Schema({"col": BaseType.VARCHAR}),
+            "b": Schema({"col": BaseType.VARCHAR}),
+        })
+        self.assertEqual(ExprConcat(ExprColumn(("b", "col")),
+                                    ExprConcat(ExprVarcharLiteral("test"), ExprColumn(("a", "col")))
+        ).type_check(st),
+        Expression(Schema({"b.col": BaseType.VARCHAR, "a.col": BaseType.VARCHAR}), BaseType.VARCHAR))
+
+    def test_concat_substr_nested(self):
+        st = SymbolTable({
+            "a": Schema({"col": BaseType.VARCHAR}),
+            "b": Schema({"col": BaseType.VARCHAR}),
+        })
+        self.assertEqual(ExprConcat(ExprColumn(("b", "col")),
+                                    ExprSubstr(ExprVarcharLiteral(
+                                        "hello world"), ExprIntLiteral(0), ExprIntLiteral(5))
+                                    
+        ).type_check(st),
+        Expression(Schema({"b.col": BaseType.VARCHAR}), BaseType.VARCHAR))
+
+
+# class TestSubstr(unittest.TestCase):
+#     def test_substr_literal(self):
+#         self.assertEqual(expr.parse('SUBSTR("test", 1, 3)'),
+#                          ExprSubstr(ExprVarcharLiteral("test"), ExprIntLiteral(1), ExprIntLiteral(3)))
+
+#     def test_substr_literal_add(self):
+#         self.assertEqual(expr.parse('SUBSTR("test", 0+1, 3)'),
+#                          ExprSubstr(ExprVarcharLiteral("test"),
+#                          ExprBinaryOp(ExprIntLiteral(0),
+#                                       BinaryOp.ADDITION,
+#                                       ExprIntLiteral(1)), ExprIntLiteral(3)))
+
+#     def test_substr_col(self):
+#         self.assertEqual(expr.parse('SUBSTR(a.col, 0+1, 3)'),
+#                          ExprSubstr(ExprColumn(("a", "col")),
+#                          ExprBinaryOp(ExprIntLiteral(0),
+#                                       BinaryOp.ADDITION,
+#                                       ExprIntLiteral(1)), ExprIntLiteral(3)))
+
+#     def test_substr_nested(self):
+#         self.assertEqual(expr.parse('SUBSTR(SUBSTR(a.col, 3, 20), 1, 5)'),
+#                          ExprSubstr(ExprSubstr(ExprColumn(("a", "col")),
+#                                                ExprIntLiteral(3),
+#                                                ExprIntLiteral(20)),  ExprIntLiteral(1), ExprIntLiteral(5)))
+
+#     def test_substr_concat(self):
+#         self.assertEqual(expr.parse('SUBSTR(CONCAT("hello", "world"), 1, 5)'),
+#                          ExprSubstr(
+#             ExprConcat(
+#                 ExprVarcharLiteral("hello"),
+#                 ExprVarcharLiteral("world")
+#             ),  ExprIntLiteral(1), ExprIntLiteral(5)))
